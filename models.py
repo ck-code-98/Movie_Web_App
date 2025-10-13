ea1ce50920c138fe5f16d10240e273f4c4b7fdd0
ck-code-98 <constantin-knappe@web.de>
2025-10-13 13:17:29 +0200

    Initiated Flask App and created Database

--- models.py
+++ models.py
@@ -4,15 +4,17 @@ db = SQLAlchemy()
 
 
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(99), nullable=False)
 
 
 class Movie(db.Model):
+    __tablename__ = 'movies'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(99), nullable=False)
     director = db.Column(db.String(99))
     year = db.Column(db.Integer)
     poster_url = db.Column(db.String)
-    user_id = db.Column(db.Integer, db.ForeignKey('user_id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
